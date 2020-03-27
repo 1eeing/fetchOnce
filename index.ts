566e198a038c1a7093456c200d237b95a5585283
leeing <lining@growingio.com>
2020-03-27 20:35:34 +0800

    bug fixed

--- index.ts
+++ index.ts
@@ -3,6 +3,11 @@ interface Opt {
   key?: string
 }
 
+interface PrmiseQueue {
+  resolve: any
+  reject: any
+}
+
 const validKey = (opt: Opt) => {
   if (['session', 'local'].includes(opt.type) && !opt.key) {
     return false;
@@ -43,37 +48,54 @@ const fetchOnce = (fn: (...args: any) => Promise<any>, opt: Opt = { type: 'memor
     throw Error('存储到storage中需要在opt中传入key');
   }
 
-  const clearQueue = () => {
-    queue.length = 0;
+  const dispatch = (isSuccess: boolean, value: any) => {
+    while (promiseQueue.length) {
+      const p = promiseQueue.shift();
+      p[isSuccess ? 'resolve' : 'reject'](value);
+    }
   }
 
-  const queue = [];
+  const fnQueue: any = [];
+  const promiseQueue: PrmiseQueue[] = [];
+  const errors: any[] = [];
   const { type, key } = opt;
   const saveRes = saveFuncMap[type];
   const getRes = getFuncMap[type];
 
   let result = getRes(key);
-  let error: any;
+  let lock = false;
 
   return function (...args: any) {
     return new Promise((resolve, reject) => {
       if (result) {
         return resolve(result);
       }
-      queue.push(fn);
-      while (queue.length) {
-        const _fn = queue.shift();
-        _fn.apply(this, args).then(res => {
-          resolve(res);
-          result = res;
-          saveRes(key, res);
-          clearQueue();
-        }).catch(e => {
-          error = e;
-        })
+      fnQueue.push(fn);
+      promiseQueue.push({ resolve, reject });
+      while (fnQueue.length && !lock) {
+        lock = true;
+        const _fn = fnQueue.shift();
+
+        // 当一个请求成功，全部都成功
+        // 当全部请求失败，才算失败
+        _fn.apply(this, args)
+          .then((res: any) => {
+            fnQueue.length = 0;
+            errors.length = 0;
+            dispatch(true, res);
+            result = res;
+            saveRes(key, res);
+          })
+          .catch((error: any) => {
+            errors.push(error);
+          })
+          .finally(() => {
+            lock = false;
+          });
       }
-      if (error) {
-        reject(error);
+      if (errors.length) {
+        dispatch(false, errors);
+        errors.length = 0;
       }
     })
   }

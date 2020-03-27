1d8d7203a1a990deb1276b18906863b535534dd3
leeing <lining@growingio.com>
2020-03-27 17:40:50 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # fetchOnce
 首屏请求前端解决方案
+
+
+## 安装
+```bash
+npm install fetchOnce --save
+```
+
+
+## 如何使用
+```js
+import fetchOnce from 'fetchOnce'
+
+const getUserInfo = () => {
+  return fetch('test.com');
+};
+
+const getUserInfoFetchOnce = fetchOnce(getUserInfo);
+
+getUserInfoFetchOnce().then(res => {
+  console.log(res);
+})
+```
+
+
+## 参数说明
+### fn
+请求函数，返回一个promise
+
+### options?
+可选配置
+
+#### options.type
+存储类型，`local | session | memory` 三个值可选，默认存储到内存中。表示存储到哪
+
+#### options.key
+当需要存储到storage中时，需要传入一个自定义key
---
+++ index.ts
@@ -0,0 +1,82 @@
+interface Opt {
+  type: 'session' | 'local' | 'memory'
+  key?: string
+}
+
+const validKey = (opt: Opt) => {
+  if (['session', 'local'].includes(opt.type) && !opt.key) {
+    return false;
+  }
+  return true;
+}
+
+const getLocalStorage = (key: string) => {
+  return window.localStorage.getItem(key);
+}
+
+const getSessionStorage = (key: string) => {
+  return window.sessionStorage.getItem(key);
+}
+
+const saveLocalStorage = (key: string, value: any) => {
+  window.localStorage.setItem(key, value);
+}
+
+const saveSessionStorage = (key: string, value: any) => {
+  window.sessionStorage.setItem(key, value);
+}
+
+const getFuncMap = {
+  local: getLocalStorage,
+  session: getSessionStorage,
+  memory: () => void 0,
+}
+
+const saveFuncMap = {
+  local: saveLocalStorage,
+  session: saveSessionStorage,
+  memory: () => {}
+}
+
+const fetchOnce = (fn: (...args: any) => Promise<any>, opt: Opt = { type: 'memory' }) => {
+  if (!validKey(opt)) {
+    throw Error('存储到storage中需要在opt中传入key');
+  }
+
+  const clearQueue = () => {
+    queue.length = 0;
+  }
+
+  const queue = [];
+  const { type, key } = opt;
+  const saveRes = saveFuncMap[type];
+  const getRes = getFuncMap[type];
+
+  let result = getRes(key);
+  let error: any;
+
+  return function (...args: any) {
+    return new Promise((resolve, reject) => {
+      if (result) {
+        return resolve(result);
+      }
+      queue.push(fn);
+      while (queue.length) {
+        const _fn = queue.shift();
+        _fn.apply(this, args).then(res => {
+          resolve(res);
+          result = res;
+          saveRes(key, res);
+          clearQueue();
+        }).catch(e => {
+          error = e;
+        })
+      }
+      if (error) {
+        reject(error);
+      }
+    })
+  }
+}
+
+export default fetchOnce;
